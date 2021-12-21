cf71a76c62954545a7d10692a480a101856eb380
Ahmed Ramy <dev.ahmedramy@gmail.com>
2021-12-21 08:23:33 +0200

    Write Test Code for Emails

--- StrategyTutorialTests/Validations/ValidationRulesTests.swift
+++ StrategyTutorialTests/Validations/ValidationRulesTests.swift
@@ -19,7 +19,36 @@ class ValidationRulesSpec: QuickSpec {
     override func spec() {
         describe("validating") {
             // MARK: - Email
-            context("emails") {}
+            context("emails") {
+                it("doesn't allow empty emails") {
+                    // Arrange
+                    let email = ""
+                    let rule = EmailValidationRule(email: email)
+                    let expectedError = LocalError(validation: ValidationError.empty(fieldName: rule.field.title)).text
+                    do {
+                        // Act
+                        try rule.validate()
+                    } catch {
+                        // Assert
+                        let actualError = error.asLocalError().text
+                        print(actualError, expectedError)
+                        expect(actualError).to(equal(expectedError), description: "\(actualError)-\(expectedError)")
+                    }
+                }
+
+                it("doesn't allow invalid emails") {
+                    let email = "..."
+                    let rule = EmailValidationRule(email: email)
+                    let expectedError = LocalError(validation: .emailInvalid).text
+                    do {
+                        try rule.validate()
+                    } catch {
+                        let actualError = error.asLocalError().text
+                        print(actualError, expectedError)
+                        expect(actualError).to(equal(expectedError), description: "\(actualError)-\(expectedError)")
+                    }
+                }
+            }
 
             // MARK: - Passwords
             context("password") {}
